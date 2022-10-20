624975169f032b2bbea9c9d2bc5e721dc7f9df2a
Jimly-Firdaus <16521312@std.stei.itb.ac.id>
2022-10-20 22:41:24 +0700

    Finished About Us, Changelog on Footer : added input email

--- src/components/About_Us.vue
+++ src/components/About_Us.vue
@@ -4,10 +4,13 @@
     <div class="section-1-about-us">
         Empowering Learners to Learn Independently at Their Own Pace.
     </div>
+    <div class="about-us-background background-no-repeat-size-contain"></div>
     <div class="about-us-dummy"></div>
     <div class="vector-5  background-no-repeat-size-contain"></div>
-
+    <div class="stars-1"></div>
+    <div class="stars-2"></div>
     <!-- Section 2 -->
+    <div class="stars-3"></div>
     <div class="section-2-about-us">
         <div class="about-us-caption">
             KiteSense™ is a cloud-based recommender that guides every learner towards mastery of a chosen subject. The AI engine scaffolds learning tasks for each subject and optimize learning paths for each learner based on pedagogical research and individual learner profile. Using data collected from formative assessments, KiteSense™ personalises the learning speed and content to every learner,  providing the right tools and support to meet their individual needs. 
@@ -51,11 +54,48 @@
     
     <!-- Section 3 -->
     <div class="vector-6 background-no-repeat-size-contain"></div>
+    <div class="vector-7 background-no-repeat-size-contain"></div>
     <div class="section-3-about-us">
-
-
+        <div class="section-3-about-us-title">
+            Meet Our Advisor Who Helped Us Get Here
+        </div>
+        <div class="section-3-about-us-content">
+            <div>
+            Professor Alex Siow is a renowned author, professor, board chairman, managing director, independent director, consultant, and advisor. Prior to joining NUS, Prof Alex was the Managing Director, Health & Public Service, Accenture, SVP/CIO, Starhub and CIO, HDB 
+            </div>
+            <div>
+            He is also an experienced independent advisory professional, with more than a decade in the industry. He regularly advises on business strategy for companies that are using or developing new ways of moving and managing technology roadmap across borders.
+            </div> 
+            <div>
+            We are very happy to have Professor Alex Siow as our advisor. He is helping us to get KiteSense better, so you can access our high-quality services and data for your personalised teaching effectively and easily.
+            </div>
+        </div>
+        <div class="section-3-about-us-profile-wrapper">
+            <div class="section-3-profile-photo-desc">
+                <DynamicLink to="https://www.linkedin.com/in/prof-alex-siow-5213b4/">
+                <div class="profile-photo background-no-repeat-size-contain">
+                    <!-- Photo here -->
+                </div> 
+                </DynamicLink>
+                <div class="section-3-about-us-name">Prof Alex Siow</div>
+                <div class="section-3-about-us-position">KiteSense’s Advisor</div>
+            </div>
+            <div class="section-3-profile-caption">Professor (Practice) at the Department of Information Systems and Analytics of the School of Computing, National University of Singapore.</div>
+        </div>
     </div>
 
+    <div class="about-us-form">
+        <div class="about-us-form-bg"></div>
+        <div class="about-us-form-title">
+            One-Stop Solution for All Need
+        </div>
+        <div class="about-us-form-caption">
+            Don’t miss out on our future updates, stay tuned to get the latest updates.
+        </div>
+        <div class="about-us-input"><input type="email" placeholder="Your Email" autocomplete="off" /></div>
+        <div class="button-style-primary-about-us"><DynamicLink to="/">Forge Mastery Now</DynamicLink></div>
+        <div class="about-us-agreement">By registering, you confirm that you agree to the storing and processing of your personal data by KiteSense as described in the <b>Terms & Policies.</b></div>
+    </div>
 </div>
 </template>
 <style src="../assets/css/About_Us.css">
