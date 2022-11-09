24f30f2d7f3b5ecbf0001f589868703fbc9b4679
JimlyF <jimly.firdaus@kitesense.io>
2022-11-09 08:43:06 +0700

    Fixed responsiveness for < 1000px

--- src/views/404page.vue
+++ src/views/404page.vue
@@ -4,6 +4,7 @@ import DynamicLink from '../components/logic/DynamicLink.vue';
 
 </script>
 <template>
+    <div class="wrapper-404">
     <div class="not-found-bg"></div>
     <div class="not-found-msg">
         Oops, looks like you're lost!
@@ -11,6 +12,7 @@ import DynamicLink from '../components/logic/DynamicLink.vue';
     <div class="not-found-dummy"></div>
     <div class="not-found-links">
         <DynamicLink to="/">Back to Homepage!</DynamicLink>
+    </div>
     </div>
 </template>
 <style src="../assets/css/not-found-page.css" scoped>
